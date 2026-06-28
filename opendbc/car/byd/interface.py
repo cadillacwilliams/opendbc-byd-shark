dd79168b08b126567cc3e82919e47c1922792db0
cadillacwilliams <blake0182@hotmail.com>
2026-06-28 08:09:24 +0800

    Update BYD opendbc

--- opendbc/car/byd/interface.py
+++ opendbc/car/byd/interface.py
@@ -12,7 +12,7 @@ class CarInterface(CarInterfaceBase):
   @staticmethod
   def _get_params(ret: structs.CarParams, candidate, fingerprint, car_fw, alpha_long, is_release, docs) -> structs.CarParams:
     ret.brand = "byd"
-    ret.dashcamOnly = True
+    ret.dashcamOnly = False
     ret.radarUnavailable = True
     ret.alphaLongitudinalAvailable = False
 
